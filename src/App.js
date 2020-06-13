b5841e2664cd2b27654c0269b1740f64ba5aa25f
Johan de Leeuw <johandeleeuw@gmail.com>
2020-06-13 23:03:56 +0200

    added searchfield and files for gh pages

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import React, { Component } from "react";
-import { CardList } from "./components/card-list/card-list.component";
-import "./App.css";
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/searchbox/searchbox.components';
+
+import './App.css';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            monsters: [
-                {
-                    id: "m1",
-                    name: "Frankenstein",
-                },
-                {
-                    id: "m2",
-                    name: "Dracula",
-                },
-                {
-                    id: "m3",
-                    name: "Voldemort",
-                },
-            ],
+            monsters: [],
+            searchField: '',
         };
     }
     componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/users")
+        fetch('https://jsonplaceholder.typicode.com/users')
             .then((response) => response.json())
             .then((users) => this.setState({ monsters: users }));
     }
+    handleChange = (e) => {
+        this.setState({ searchField: e.target.value });
+    };
+
     render() {
+        const { monsters, searchField } = this.state;
+        const filteredMonsters = monsters.filter((monster) =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        );
         return (
             <div className="App">
-                <CardList monsters={this.state.monsters} />
+                <h1>Monsters Rolodex</h1>
+                <SearchBox
+                    placeholder="Search Monsters"
+                    handleChange={this.handleChange}
+                />
+                <CardList monsters={filteredMonsters} />
             </div>
         );
     }
